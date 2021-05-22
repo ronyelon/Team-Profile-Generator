c0fd6b7f105067ec215733d8d88138828cdfcab1
Rony Braswell <ronybraswell@hotmail.com>
2021-05-22 10:47:31 -0400

    Updating html outputs

--- index.js
+++
@@ -1,8 +0,0 @@
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
-
-const Manager = require("./lib/manager");
-const Engineer = require("./lib/engineer");
-const Intern = require("./lib/intern");
-const Employee = require("./lib/employee");
